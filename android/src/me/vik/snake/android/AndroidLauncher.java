2e11a2890e3f4b078e0601d02bfa689d09131fd9
Vik Goel <vgoel@live.com>
2014-07-29 23:57:09 -0400

    Initial Commit

---
+++ android/src/me/vik/snake/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package me.vik.snake.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import me.vik.snake.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
---
+++ desktop/src/me/vik/snake/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.vik.snake.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.vik.snake.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
